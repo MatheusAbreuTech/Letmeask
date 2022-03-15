a3874af07bd12f7e2a55d0169a8a02761f72b897
Matheus Abreu <theus.santosab@gmail.com>
2022-03-14 22:41:13 -0300

    update

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
-import "firebase/auth";
-import "firebase/database";
+import 'firebase/auth';
+import 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,7 +11,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
