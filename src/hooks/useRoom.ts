60ff537cf8496c2329d621ae6dc80fb0e6767734
Matheus Abreu <theus.santosab@gmail.com>
2022-03-24 16:45:31 -0300

    likes function on questions completed

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,16 +1,6 @@
-import { getDatabase, onValue, ref } from 'firebase/database';
+import { getDatabase, onValue, ref, off } from 'firebase/database';
 import { useEffect, useState } from 'react';
-
-type QuestionType = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-};
+import { useAuth } from './useAuth';
 
 type FirebaseQuestions = Record<
   string,
@@ -22,10 +12,30 @@ type FirebaseQuestions = Record<
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<
+      string,
+      {
+        authorId: string;
+      }
+    >;
   }
 >;
 
+type QuestionType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  likesCount: number;
+  likeId: string | undefined;
+};
+
 export default function useRoom(roomId: string | undefined) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -34,7 +44,7 @@ export default function useRoom(roomId: string | undefined) {
     const roomRef = ref(db, `rooms/${roomId}`);
     onValue(roomRef, (room) => {
       const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.question ?? {};
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
       const parsedQuestions = Object.entries(firebaseQuestions).map(
         ([key, value]) => {
@@ -43,7 +53,11 @@ export default function useRoom(roomId: string | undefined) {
             content: value.content,
             author: value.author,
             isHighlighted: value.isHighlighted,
-            isAnswered: value.isAnswered
+            isAnswered: value.isAnswered,
+            likesCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0]
           };
         }
       );
@@ -51,7 +65,11 @@ export default function useRoom(roomId: string | undefined) {
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     });
-  }, [roomId]);
+
+    return () => {
+      off(roomRef);
+    };
+  }, [roomId, user?.id]);
 
   return { questions, title };
 }
